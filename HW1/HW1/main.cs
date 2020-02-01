15b9d9ae024b7feeff7b8a9c3f41817c0eadc89b
Tala <tala.karaki@wsu.edu>
2020-01-31 20:42:20 -0800

    finished menu functions

--- HW1/HW1/main.cs
+++ HW1/HW1/main.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace HW1
 {
-    class Program
+    class main
     {
+        HW1.BST myBST = new HW1.BST();
+
         static void Main(string[] args)
         {
             int Exited = 0;
@@ -16,7 +18,7 @@ namespace HW1
             {
 
 
-                Program p = new Program();
+                main p = new main();
                 p.PrintMenu();
                 Exited = p.ReadMenu();
             }
@@ -26,8 +28,128 @@ namespace HW1
             Console.ReadKey();
         }
 
-        void PrintMenu() { }
 
-        int ReadMenu() { }
+
+        /* Prints a menu to run or exit the program */
+        public void PrintMenu()
+        {
+            Console.Write("\n\n\n\n====================");
+            Console.Write("\nWelcome to the BST Calculator");
+            Console.Write("\n\n\t 1. Run the program");
+            Console.Write("\n\n\t 0. Exit");
+            Console.Write("\n\n====================");
+            Console.Write("\nEnter Your Choice (0-1):");
+        }
+
+
+
+        /* Runs user input from menu options */
+        public int ReadMenu()
+        {
+
+            int exit = 0;
+            int caseSwitch = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.Write("\n\n ...Running Program ...\n\n");
+                    Console.WriteLine("\nEnter your numbers on a single line, ranging from 0 to 100, separated by spaces:");
+                    ParseInput();
+                    Console.WriteLine("\n\n\n ... Creating BST... ");
+                    Console.WriteLine("\nBST Items:");
+                    DisplayBST();
+                    Console.WriteLine("\nBST Stats:");
+                    DisplayStats();
+                    break;
+
+                case 0:
+                    Console.WriteLine("\nMenu Exited.");
+                    exit = 1;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+
+            return exit;
+        }
+
+
+
+        /* Reads a collection of numbers from user and parses data into BST */
+        public void ParseInput()
+        {
+            // obtain user input
+            string input = "";
+            input = Console.ReadLine();
+
+            //start parsing by inserting into a list
+            int num;
+            List<int> NumList = new List<int>();
+
+
+            if (input != "")
+            {
+                char[] separator = { ' ' };
+                String[] strlist = input.Split(separator);
+
+                foreach (String s in strlist)
+                {
+                    if (s != "")
+                    {
+                        num = Convert.ToInt32(s);
+                        if (num > 0 && num < 100 && !NumList.Contains(num))
+                        {
+                            NumList.Add(num);
+                        }
+                    }
+                }
+
+            }
+
+            //Print accepted numbers
+            Console.Write("\nThe following numbers were saved:");
+            foreach (Int32 element in NumList)
+            {
+                Console.Write(element + " ");
+            }
+
+
+            //insert into BST
+            CreateBST(NumList);
+
+        }
+
+
+
+        /* Inserts parsed numbers from list into the BST */
+        public void CreateBST(List<int> myList)
+        {
+            foreach (Int32 element in myList)
+            {
+                myBST.Add(element);
+            }
+        }
+
+
+
+        /* Displays contents of BST from smallest to largest */
+        public void DisplayBST()
+        {
+            myBST.Print();
+        }
+
+
+
+        /* Displays the stats of the BST: Node count, Level Count, Theoritical Minimum */
+        public void DisplayStats()
+        {
+            Console.WriteLine("\t# of Items:" + myBST.NodeCount());
+            Console.WriteLine("\t# of Levels:" + myBST.BSTLevels());
+            Console.WriteLine("\tTheoretical Minimum # of Levels:" + myBST.MinLevels());
+        }
     }
 }
