6a340b2f9635e82b017842bddc492d6e8046005e
Tala <tala.karaki@wsu.edu>
2020-02-21 17:40:39 -0800

    designed winforms app

---
+++ HW3/HW3/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace HW3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFromFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFibonacciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.first50ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.first100ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(793, 418);
+            this.textBox1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFromFileToolStripMenuItem,
+            this.saveToFileToolStripMenuItem,
+            this.loadFibonacciToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadFromFileToolStripMenuItem
+            // 
+            this.loadFromFileToolStripMenuItem.Name = "loadFromFileToolStripMenuItem";
+            this.loadFromFileToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.loadFromFileToolStripMenuItem.Text = "Load from File";
+            // 
+            // saveToFileToolStripMenuItem
+            // 
+            this.saveToFileToolStripMenuItem.Name = "saveToFileToolStripMenuItem";
+            this.saveToFileToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.saveToFileToolStripMenuItem.Text = "Save to File";
+            // 
+            // loadFibonacciToolStripMenuItem
+            // 
+            this.loadFibonacciToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.first50ToolStripMenuItem,
+            this.first100ToolStripMenuItem});
+            this.loadFibonacciToolStripMenuItem.Name = "loadFibonacciToolStripMenuItem";
+            this.loadFibonacciToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.loadFibonacciToolStripMenuItem.Text = "Load Fibonacci";
+            // 
+            // first50ToolStripMenuItem
+            // 
+            this.first50ToolStripMenuItem.Name = "first50ToolStripMenuItem";
+            this.first50ToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.first50ToolStripMenuItem.Text = "First 50";
+            // 
+            // first100ToolStripMenuItem
+            // 
+            this.first100ToolStripMenuItem.Name = "first100ToolStripMenuItem";
+            this.first100ToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.first100ToolStripMenuItem.Text = "First 100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFromFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFibonacciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem first50ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem first100ToolStripMenuItem;
+    }
+}
+
