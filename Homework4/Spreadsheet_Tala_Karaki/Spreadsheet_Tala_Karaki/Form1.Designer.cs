fc3c9611d1f3bd157c277b84024d92aab6b4a6dd
Tala <tala.karaki@wsu.edu>
2020-03-01 20:39:02 -0800

    added engine to spreadsheet winform

--- Homework4/Spreadsheet_Tala_Karaki/Spreadsheet_Tala_Karaki/Form1.Designer.cs
+++ Homework4/Spreadsheet_Tala_Karaki/Spreadsheet_Tala_Karaki/Form1.Designer.cs
@@ -29,23 +29,35 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DemoButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(797, 448);
+            this.dataGridView1.Size = new System.Drawing.Size(1166, 681);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // DemoButton
+            // 
+            this.DemoButton.Location = new System.Drawing.Point(456, 708);
+            this.DemoButton.Name = "DemoButton";
+            this.DemoButton.Size = new System.Drawing.Size(257, 30);
+            this.DemoButton.TabIndex = 1;
+            this.DemoButton.Text = "Demo";
+            this.DemoButton.UseVisualStyleBackColor = true;
+            this.DemoButton.Click += new System.EventHandler(this.DemoButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1166, 750);
+            this.Controls.Add(this.DemoButton);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Spreadsheet";
@@ -58,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button DemoButton;
     }
 }
 
--- Homework4/Spreadsheet_Tala_Karaki/Spreadsheet_Tala_Karaki/Form1.cs
+++ Homework4/Spreadsheet_Tala_Karaki/Spreadsheet_Tala_Karaki/Form1.cs
@@ -1,43 +1,111 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿/*
+*    Name: Tala Karaki
+*    Id: 11659015
+*/
 
 namespace Spreadsheet_Tala_Karaki
 {
+    using System;
+    using System.ComponentModel;
+    using System.Windows.Forms;
+    using Spreadsheet_Engine;
+
     public partial class Form1 : Form
     {
+        private SpreadsheetEngine spreadSheet1;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpreadsheetForm"/> class.
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Handles SpreadsheetForm's Load.
+        /// </summary>
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-            this.dataGridView1.ColumnCount = 26;
+            this.spreadSheet1 = new SpreadsheetEngine(50, 26);
+            this.spreadSheet1.CellPropertyChanged += this.CellPropertyChanged;
+            this.InitializeGrid();
+        }
 
-             char c = 'A';
-            for (int i = 0; i < 26; i++)
-            {
-                this.dataGridView1.Columns[i].Name = c.ToString();
-                c++;
-            }
+        /// <summary>
+        /// Creates and names the rows and columns of the dataGridView.
+        /// </summary>
+        public void InitializeGrid()
+        {
+            this.dataGridView1.RowCount = this.spreadSheet1.RowCount;
+            this.dataGridView1.ColumnCount = this.spreadSheet1.ColumnCount;
 
-            this.dataGridView1.RowCount = 50;
-            for (int i = 1; i <= 50; i++)
+            for (int i = 1; i <= spreadSheet1.RowCount; i++)
             {
                 this.dataGridView1.Rows[i - 1].HeaderCell.Value = i.ToString();
             }
 
-           
+            char c = 'A';
+            for (int i = 0; i < spreadSheet1.ColumnCount; i++)
+            {
+                this.dataGridView1.Columns[i].Name = c.ToString();
+                c++;
+            }
+        }
+
+        /// <summary>
+        /// Event handler for updating a cell’s value in the DataGridView and SpreadsheetEngine.
+        /// </summary>
+        private void CellPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Cell cell = (Cell)sender;
+            if (e.PropertyName == "Value")
+            {
+                this.dataGridView1.Rows[cell.RowIndex].Cells[cell.ColumnIndex].Value = cell.Value;
+            }
+        }
 
 
+        /// <summary>
+        /// Handles what happens when the Demo button is clicked.
+        /// </summary>
+        private void DemoButton_Click(object sender, EventArgs e)
+        {
+            this.SpreadsheetDemo();
+        }
+
+       
+        /// <summary>
+        /// Runs a Demo of the spreadsheet form.
+        /// </summary>
+        private void SpreadsheetDemo()
+        {
+            // set the text in about 50 random cells
+            Random rnd = new Random();
+            int rIndex = 0;
+            int cIndex = 0;
+            for (int i = 0; i < 50; i++)
+            {
+                rIndex = rnd.Next(0, this.spreadSheet1.RowCount);
+                cIndex = rnd.Next(0, this.spreadSheet1.ColumnCount);
+                this.spreadSheet1.GetCell(rIndex, cIndex).Text = "Hello World!";
+            }
+
+            // set the text in every cell in column B to “This is cell B#”
+            for (int i = 0; i < this.spreadSheet1.RowCount; i++)
+            {
+                this.spreadSheet1.GetCell(i, 1).Text = "This is Cell B" + (i + 1).ToString();
+            }
+
+            // set the text in every cell in column A to “=B#”
+            for (int i = 0; i < this.spreadSheet1.RowCount; i++)
+            {
+                spreadSheet1.GetCell(i, 0).Text = "=B" + (i + 1).ToString();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
         }
     }
 }
