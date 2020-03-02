7b8a6301cfeb4ab56381da715c504d97e4cca69d
Tala <tala.karaki@wsu.edu>
2020-03-01 20:15:00 -0800

    implemented spreadsheet engine

--- Homework4/Spreadsheet_Tala_Karaki/Spreadsheet_Engine/SpreadsheetCell.cs
+++ Homework4/Spreadsheet_Tala_Karaki/Spreadsheet_Engine/SpreadsheetCell.cs
@@ -9,28 +9,95 @@ namespace Spreadsheet_Engine
     using System.ComponentModel;
 
     public abstract class SpreadsheetCell : INotifyPropertyChanged
+    {
+        protected string text = string.Empty;
+        protected string value = string.Empty;
+        private readonly int rowIndex = 0;
+        private readonly int columnIndex = 0;
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        /// <summary>
+        /// Constructor for Abstract SpreadsheetCell class.
+        /// </summary>
+        /// <param name="rIndex">cell row index</param>
+        /// <param name="cIndex"> cell column index</param>
+        /// <param name="text"> cell text contents</param>
+        public SpreadsheetCell(int rIndex, int cIndex, string text)
         {
-            protected string text = string.Empty;
-            protected string value = string.Empty;
-            private readonly int rowIndex = 0;
-            private readonly int columnIndex = 0;
+            this.rowIndex = rIndex;
+            this.columnIndex = cIndex;
+            this.text = text;
+            this.value = text;
+        }
 
-            public event PropertyChangedEventHandler PropertyChanged = delegate { };
 
-            /// <summary>
-            /// Constructor for Abstract SpreadsheetCell class.
-            /// </summary>
-            /// <param name="rIndex">cell row index</param>
-            /// <param name="cIndex"> cell column index</param>
-            /// <param name="text"> cell text contents</param>
-            public SpreadsheetCell(int rIndex, int cIndex, string text)
+        public int RowIndex
+        {
+            get
             {
-                this.rowIndex = rIndex;
-                this.columnIndex = cIndex;
-                this.text = text;
-                this.value = text;
+                return this.rowIndex;
+            }
+        }
+
+        public int ColumnIndex
+        {
+            get
+            {
+                return this.columnIndex;
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return this.text;
             }
 
+            set
+            {
+                if (value != this.text)
+                {
+                    this.text = value;
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("Text"));
+                }
+            }
+        }
 
+        public string Value
+        {
+            get
+            {
+                return this.value;
+            }
+
+            protected internal set
+            {
+                if (value != this.value)
+                {
+                    this.value = value;
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("Value"));
+                }
+            }
         }
     }
+
+    // Cell Class inherited from SpreadSheet to be instantiated in Spreadsheet
+    public class Cell : SpreadsheetCell
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cell"/> class.
+        /// Constructor for inherited Cell class.
+        /// </summary>
+        /// <param name="rIndex">cell row index.</param>
+        /// <param name="cIndex"> cell column index.</param>
+        /// <param name="text"> cell text contents.</param>
+        public Cell(int rindex, int cindex, string name)
+                : base(rindex, cindex, name)
+        {
+        }
+    }
+
+}
+
--- Homework4/Spreadsheet_Tala_Karaki/Spreadsheet_Engine/SpreadsheetEngine.cs
+++ Homework4/Spreadsheet_Tala_Karaki/Spreadsheet_Engine/SpreadsheetEngine.cs
@@ -6,7 +6,101 @@ using System.Threading.Tasks;
 
 namespace Spreadsheet_Engine
 {
+    using System;
+    using System.ComponentModel;
+
     public class SpreadsheetEngine
     {
+        protected Cell[,] cellArray;
+        private readonly int rowCount = 0;
+        private readonly int columnCount = 0;
+
+        public event PropertyChangedEventHandler CellPropertyChanged;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Spreadsheet"/> class.
+        /// Constructor creates a 2D array of Cells with rowCount and columnCount size.
+        /// </summary>
+        /// <param name="rowC"> Row count.</param>
+        /// <param name="colC"> Column count.</param>
+        public SpreadsheetEngine(int rowC, int colC)
+        {
+            this.rowCount = rowC;
+            this.columnCount = colC;
+
+            this.cellArray = new Cell[rowC, colC];
+
+            for (int i = 0; i < rowC; i++)
+            {
+                for (int j = 0; j < colC; j++)
+                {
+                    this.cellArray[i, j] = new Cell(i, j, string.Empty);
+                    this.cellArray[i, j].PropertyChanged += this.OnPropertyChanged;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the Cell at rowIndex and columnIndex in cellArray.
+        /// Returns Null if indexes are out of bounds.
+        /// </summary>
+        /// <param name="rowIndex"> Row count.</param>
+        /// <param name="columnIndex"> Column count.</param>
+        public Cell GetCell(int rowIndex, int columnIndex)
+        {
+            if (rowIndex > this.rowCount || rowIndex < 0 ||
+                columnIndex > this.columnCount || columnIndex < 0)
+            {
+                return null;
+            }
+            else
+            {
+                return this.cellArray[rowIndex, columnIndex];
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of rows in cellArray.
+        /// </summary>
+        public int RowCount
+        {
+            get
+            {
+                return this.rowCount;
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of columns in cellArray.
+        /// </summary>
+        public int ColumnCount
+        {
+            get
+            {
+                return this.columnCount;
+            }
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Text")
+            {
+                if (((Cell)sender).Text.StartsWith("="))
+                {
+                    char columnLetter = ((Cell)sender).Text[1];
+                    int columnIndex = char.ToUpper(columnLetter) - 65;
+                    int rowIndex = int.Parse(((Cell)sender).Text.Substring(2)) - 1;
+                    ((Cell)sender).Value = this.GetCell(rowIndex, columnIndex).Value;
+                }
+                else
+                {
+                    ((Cell)sender).Value = ((Cell)sender).Text;
+                }
+            }
+
+            this.CellPropertyChanged?.Invoke(sender, new PropertyChangedEventArgs("Value"));
+        }
     }
 }
+
+
