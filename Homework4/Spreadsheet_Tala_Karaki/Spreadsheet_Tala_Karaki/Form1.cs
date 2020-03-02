8d8eb2f8819f46e6d660fb9436989e640adee38f
Tala <tala.karaki@wsu.edu>
2020-03-01 19:53:51 -0800

    added columns and rows

--- Homework4/Spreadsheet_Tala_Karaki/Spreadsheet_Tala_Karaki/Form1.cs
+++ Homework4/Spreadsheet_Tala_Karaki/Spreadsheet_Tala_Karaki/Form1.cs
@@ -16,5 +16,28 @@ namespace Spreadsheet_Tala_Karaki
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.dataGridView1.ColumnCount = 26;
+
+             char c = 'A';
+            for (int i = 0; i < 26; i++)
+            {
+                this.dataGridView1.Columns[i].Name = c.ToString();
+                c++;
+            }
+
+            this.dataGridView1.RowCount = 50;
+            for (int i = 1; i <= 50; i++)
+            {
+                this.dataGridView1.Rows[i - 1].HeaderCell.Value = i.ToString();
+            }
+
+           
+
+
+        }
     }
 }
