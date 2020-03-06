9343c0d8116339a957a786bb48a87202bf478076
Tala <tala.karaki@wsu.edu>
2020-03-06 11:18:22 -0800

    created empty classes for expressiontree and tests

---
+++ Spreadsheet_Tala_Karaki/Spreadsheet_Engine/ExpressionTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spreadsheet_Engine
+{
+    class ExpressionTree
+    {
+        public ExpressionTree(string expression) { }
+
+        public void SetVariable(string variableName, double variableValue) { }
+
+        public double Evaluate() { }
+    }
+}
---
+++ Spreadsheet_Tala_Karaki/Spreadsheet_Tala_Karaki/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Spreadsheet_Tala_Karaki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DemoButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1166, 681);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // DemoButton
+            // 
+            this.DemoButton.Location = new System.Drawing.Point(456, 708);
+            this.DemoButton.Name = "DemoButton";
+            this.DemoButton.Size = new System.Drawing.Size(257, 30);
+            this.DemoButton.TabIndex = 1;
+            this.DemoButton.Text = "Demo";
+            this.DemoButton.UseVisualStyleBackColor = true;
+            this.DemoButton.Click += new System.EventHandler(this.DemoButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 750);
+            this.Controls.Add(this.DemoButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Spreadsheet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button DemoButton;
+    }
+}
+
