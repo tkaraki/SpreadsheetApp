1278a7600e669d075f9ce52915940ba67615cf1f
Tala <tala.karaki@wsu.edu>
2020-03-06 22:41:25 -0800

    fixed tests

--- Spreadsheet_Tala_Karaki/SpreadsheetTests/ExpressionTreeTests.cs
+++ Spreadsheet_Tala_Karaki/SpreadsheetTests/ExpressionTreeTests.cs
@@ -13,17 +13,19 @@ namespace SpreadsheetTests
         public void TestMethod()
         {
             // Testing multiple expressions
-            string answer1 = "ABHello6";
-            Spreadsheet_Engine.ExpressionTree expression1 = new Spreadsheet_Engine.ExpressionTree("A+B+C1+Hello+6");
+            double answer1 = 66;
+            Spreadsheet_Engine.ExpressionTree expression1 = new Spreadsheet_Engine.ExpressionTree("A+B+6");
+            expression1.SetVariable("A", 30);
+            expression1.SetVariable("B", 30);
             Assert.AreEqual(answer1, expression1.Evaluate());
 
             // Testing multiple expressions
-            string answer2 = "15";
+            double answer2 = 15;
             Spreadsheet_Engine.ExpressionTree expression2 = new Spreadsheet_Engine.ExpressionTree("4+5+6");
             Assert.AreEqual(answer2, expression2.Evaluate());
 
             // Testing multiple expressions
-            string answer3 = "97";
+            double answer3 = 97;
             Spreadsheet_Engine.ExpressionTree expression3 = new Spreadsheet_Engine.ExpressionTree("100-1-2");
             Assert.AreEqual(answer3, expression3.Evaluate());
 
